82fab2159f8ec35d2566e593ccd460db957fa16c
Sasha <st134267@student.spbu.ru>
2024-09-25 07:30:07 +0000

    improved circle_area and sum_or_mult scripts

--- .ipynb_checkpoints/compare_input_with_given_list-checkpoint.py
+++ .ipynb_checkpoints/compare_input_with_given_list-checkpoint.py
@@ -3,9 +3,9 @@ print(list_to_compare)
 size = int(input('size: '))
     
 list_one = [] #empty so that we could append 2 strings
-for i in range(size): #repeats cycle i times for i values
-    temp = int(input(f'{i+1}th value: ')) # we add +1, otherwise it starts with 0
-    list_one.append(temp)    #adds i values repeatedly to an empty string
+for i in range(size): #repeasts cycle i times for i values
+   temp = int(input(f'{i+1}th value: ')) # we add +1, otherwise it starts with 0
+   list_one.append(temp)    #adds i values repeatedly to an empty string
 
 common = [val for val in list_to_compare if val == val in list_one]
 print(common)
