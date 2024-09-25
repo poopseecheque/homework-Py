2cbbdc0ec0a61a9532f2c742891d3081bcbf1110
Kate <st133145@gsom.spbu.ru>
2024-09-25 07:04:14 +0000

    add 3 scripts in py

---
+++ circle_area.py
@@ -0,0 +1,7 @@
+radius = int(input("Input circle radius:" ))
+pivalue = float(3.141592653589793)
+
+if radius >=0:
+  print("cicle area =", pivalue*radius*radius)
+else:
+  print("radius must be a non negative number")
---
+++ sum_or_mult.py
@@ -0,0 +1,7 @@
+a = int(input('enter 1st value: '))
+b = int(input('enter 2nd value: '))
+
+if a + b >= 50:
+  print("1st + 2nd =", a + b)
+else:
+  print("1st * 2nd =", a * b)
